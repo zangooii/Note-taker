f2ca90fe6274f90fdac576cb73a1f14ea4d92b11
Amirhossein <amirhosseinzangooiizadeh@gmail.com>
2022-10-03 14:48:21 +0330

    start for multiple notes

--- notetaker.js
+++ notetaker.js
@@ -1,6 +1,30 @@
-let input = "";
+let myNote = "";
+let myTitle = "";
+
+let noteInput = document.getElementById("note-input");
+let titleInput = document.getElementById("title-input");
+
+let title = document.getElementById("title");
+let note = document.getElementById("first-note-p");
+
+let addButton = document.getElementById("addButton");
+let removeButton = document.getElementById("remove-button");
 
 const addNote = () => {
-  input = document.getElementById("input").value;
-  document.getElementById("first-note").innerHTML = input;
+  myNote = noteInput.value;
+  myTitle = titleInput.value;
+
+  title.innerHTML = myTitle;
+  note.innerText = myNote;
+
+  titleInput.value = "";
+  noteInput.value = "";
 };
+
+const removeNote = () => {
+  title.innerHTML = "";
+  note.innerHTML = "";
+};
+
+removeButton.onclick = removeNote;
+addButton.onclick = addNote;
